f85b7293064d76eb26f258d1bb300644547afa79
lucaszkmtw <lucasnoirat@gmail.com>
2021-10-07 08:58:15 -0300

    consumidores grupales

--- Channel/views.py
+++ Channel/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request,'index.html')
+    return render(request,'chat/index.html')
 
 
 def room(request, room_name):
--- django_channel/asgi.py
+++ django_channel/asgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
-
+from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter
+from channels.routing import  URLRouter
+import Channel.routing
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_channel.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+  "http": get_asgi_application(),
+  "websocket": AuthMiddlewareStack(
+        URLRouter(
+            Channel.routing.websocket_urlpatterns
+        )
+    ),
+})
