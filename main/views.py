99534227a748b4c45b5db25f01fa3e78344bd3f7
lucaszkmtw <lucasnoirat@gmail.com>
2021-10-24 18:54:23 -0300

    creacion de usuario

--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
+from django.http.response import JsonResponse
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -12,5 +14,8 @@ def home(request):
 def profile(request):
     return render(request, 'profile/profile.html')
 
-
+def nuevo_usaurio(request):
+    if request.method == 'POST':
+        pass
+    return JsonResponse
 
